6b34a26c78876d9fe21b0b4ef6fefa6c61570c58
severinocarlos <juninho_456@hotmail.com>
2023-05-08 17:17:23 -0300

    style: refactoring on classes and styles

--- src/script/index.js
+++ src/script/index.js
@@ -5,19 +5,19 @@ const colors =  {
 }
 const add_btn = document.querySelector('#btn-add-task');
 
-const addCard = (_list, _taks, _priority) =>  {
+const addCard = (_list, _tasks, _priority) =>  {
     const item = document.createElement('li')
-    const text = document.createTextNode(_taks.value)
+    const text = document.createTextNode(_tasks.value)
     item.appendChild(text)
     item.style.background = colors[_priority.value]
     _list.appendChild(item);
 }
 
 add_btn.addEventListener('click', () => {
-    const list = document.querySelector('.list-in-progress-tasks')
+    const list = document.querySelector('.in-progress-tasks-list')
     const task = document.querySelector('#input-task')
-    const priority = document.querySelector('#selec-item')
-    
+    const priority = document.querySelector('#select-item')
+
     addCard(list, task, priority)
     
     task.value = ''
