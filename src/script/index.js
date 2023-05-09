a87db924edd93351282051f98571c5b5edda62fc
severinocarlos <juninho_456@hotmail.com>
2023-05-09 17:47:21 -0300

    feat: adding card removal

--- src/script/index.js
+++ src/script/index.js
@@ -4,10 +4,14 @@ const colors =  {
     'High': '#C21010'
 }
 const key = 'tasks'
-const add_btn = document.querySelector('#btn-add-task');
+const addButton = document.querySelector('#btn-add-task');
 
 const getItems = () => {
     return JSON.parse(localStorage.getItem(key) || '[]');
+}
+
+const setItems = (_tasks) => {
+    localStorage.setItem(key, JSON.stringify(_tasks));
 }
 
 const showCards = () =>  {
@@ -16,12 +20,21 @@ const showCards = () =>  {
     list.innerHTML = ''
     
     tasks.forEach(task => {
-        console.log(colors[task.priority])
-        list.innerHTML += `<li style="background:${colors[task.priority]};">${task.task}</li>`
+        list.innerHTML += `<li style="background:${colors[task.priority]};">
+                                ${task.task_name}
+                                <div >
+                                    <button id="btn-move-to-trash" onclick="removeCard('${task.task_name}')" style="background: transparent; border:0; cursor: pointer; outline: none;">
+                                        <img style="width:1rem; height: 1rem; margin-right: 1rem;"src="${'./assets/img/cross.png'}" alt="Move to trash"/>
+                                    </button>
+                                    <button id="btn-move-to-done" onclick="doneCard('${task.task_name}')" style="background: transparent; border:0; cursor: pointer; outline: none;">
+                                        <img style="width:1rem; height: 1rem;"src="${'./assets/img/check.png'}" alt="Done task"/>
+                                    </button>
+                                <div>
+                            </li>`
     });
 }
 
-add_btn.addEventListener('click', () => {
+addButton.addEventListener('click', () => {
     const task = document.querySelector('#input-task')
     const priority = document.querySelector('#select-item')
 
@@ -30,13 +43,32 @@ add_btn.addEventListener('click', () => {
     } else {
         let tasks = getItems()
         tasks.push({
-            task: task.value,
+            task_name: task.value,
             priority: priority.value
         })
-        localStorage.setItem(key, JSON.stringify(tasks));
+        setItems(tasks)
         showCards()
     }
     task.value = ''
 })
+
+const getCard = (tasks, titleCard) => {
+    let index = tasks.findIndex(task => task.task_name == titleCard)
+    return index, tasks
+}
+
+const removeCard = (_name, _tasks) => {
+    let tasks = getItems()
+    let index = getCard(tasks, _name)
+    tasks.splice(index, 1);
+    setItems(tasks)
+    showCards()
+}
+
+// const doneCard = (_name) => {
+//     let tasks = getItems()
+//     let index = tasks.findIndex(task => task.task_name == _name)
+
+// }
 
 showCards()
