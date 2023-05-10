d2c6c2fdc53190a80dfa49a5bbf97a2b751c7895
severinocarlos <juninho_456@hotmail.com>
2023-05-10 14:53:24 -0300

    feat: move card to completed list

--- src/script/index.js
+++ src/script/index.js
@@ -1,37 +1,56 @@
 const colors =  {
-    'Low': '#293462',
-    'Medium': '#FEB139',
-    'High': '#C21010'
+    'Low': '#003049',
+    'Medium': '#f77f00',
+    'High': '#d62828'
 }
-const key = 'tasks'
+const inProgressTasksKey = 'inProgressTasks'
+const doneTasksKey = 'DoneTasks'
 const addButton = document.querySelector('#btn-add-task');
 
 const getItems = () => {
-    return JSON.parse(localStorage.getItem(key) || '[]');
+    return  {
+                inProgressTasks: JSON.parse(localStorage.getItem(inProgressTasksKey) || '[]'), 
+                doneTasks: JSON.parse(localStorage.getItem(doneTasksKey) || '[]')
+            }
 }
 
-const setItems = (_tasks) => {
+const setItems = (_tasks, key) => {
     localStorage.setItem(key, JSON.stringify(_tasks));
 }
 
 const showCards = () =>  {
-    const list = document.querySelector('.in-progress-tasks-list')
-    let tasks = getItems()
-    list.innerHTML = ''
-    
-    tasks.forEach(task => {
-        list.innerHTML += `<li style="background:${colors[task.priority]};">
+    const inProgressList = document.querySelector('.in-progress-tasks-list')
+    const doneList = document.querySelector('.done-tasks-list')
+    let { inProgressTasks, doneTasks } = getItems()
+
+    inProgressList.innerHTML = ''
+    doneList.innerHTML = ''
+    const day = new Date();
+
+    inProgressTasks.forEach(task => {
+        inProgressList.innerHTML += `<li style="background:${colors[task.priority]}; word-wrap: wrap;">task:<br>
                                 ${task.task_name}
-                                <div >
-                                    <button id="btn-move-to-trash" onclick="removeCard('${task.task_name}')" style="background: transparent; border:0; cursor: pointer; outline: none;">
+                                <div class='btns'>
+                                    <button id="btn-move-to-trash" onclick="removeCard('${task.task_name}')" style="background: transparent; border:0; cursor: pointer; outline: none; margin-bottom: 1rem; padding-right: 0.5rem;">
                                         <img style="width:1rem; height: 1rem; margin-right: 1rem;"src="${'./assets/img/cross.png'}" alt="Move to trash"/>
                                     </button>
                                     <button id="btn-move-to-done" onclick="doneCard('${task.task_name}')" style="background: transparent; border:0; cursor: pointer; outline: none;">
                                         <img style="width:1rem; height: 1rem;"src="${'./assets/img/check.png'}" alt="Done task"/>
                                     </button>
+                                    <p>${day.toLocaleDateString('pt-PT')}</p>
                                 <div>
-                            </li>`
+                                </li>`
+
+        
     });
+
+    doneTasks.forEach(task => {
+        doneList.innerHTML += `<li style="background:${colors[task.priority]}; word-wrap: wrap;">
+                                    ${task.task_name}
+                                    <p>${day.toLocaleDateString('pt-PT')}</p>
+                                </li>`
+    })
+
 }
 
 addButton.addEventListener('click', () => {
@@ -41,12 +60,12 @@ addButton.addEventListener('click', () => {
     if (!task.value) {
         alert('Please, write some task')
     } else {
-        let tasks = getItems()
-        tasks.push({
+        let { inProgressTasks, doneTasks } = getItems()
+        inProgressTasks.push({
             task_name: task.value,
             priority: priority.value
         })
-        setItems(tasks)
+        setItems(inProgressTasks, inProgressTasksKey)
         showCards()
     }
     task.value = ''
@@ -54,21 +73,32 @@ addButton.addEventListener('click', () => {
 
 const getCard = (tasks, titleCard) => {
     let index = tasks.findIndex(task => task.task_name == titleCard)
-    return index, tasks
+    return index
 }
 
 const removeCard = (_name, _tasks) => {
-    let tasks = getItems()
-    let index = getCard(tasks, _name)
-    tasks.splice(index, 1);
-    setItems(tasks)
+    let { inProgressTasks, doneTasks } = getItems()
+    let index = getCard(inProgressTasks, _name)
+    console.log(index)
+    inProgressTasks.splice(index, 1);
+    setItems(inProgressTasks, inProgressTasksKey)
     showCards()
 }
 
-// const doneCard = (_name) => {
-//     let tasks = getItems()
-//     let index = tasks.findIndex(task => task.task_name == _name)
+const doneCard = (_name) => {
+    let {inProgressTasks, doneTasks} = getItems()
+    let index = inProgressTasks.findIndex(task => task.task_name == _name)
+    let titleCard = inProgressTasks[index].task_name
+    let priority = inProgressTasks[index].priority
 
-// }
+    doneTasks.push({
+        task_name: titleCard,
+        priority: priority
+    })
+    removeCard(titleCard, inProgressTasks)
+    setItems(doneTasks, doneTasksKey);
+    showCards()
+}
+
 
 showCards()
