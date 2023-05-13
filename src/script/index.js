1cefb8c28c3e26a6dc6ea442f3c3506a65fa197f
severinocarlos <juninho_456@hotmail.com>
2023-05-13 12:39:19 -0300

    feat: fix in board

--- src/script/index.js
+++ src/script/index.js
@@ -19,6 +19,7 @@ const addButton = document.querySelector('#btn-add-task');
 
 const getItems = () => {
     return  {
+                toDoTasks: JSON.parse(localStorage.getItem(toDoTasksKey) || '[]'), 
                 inProgressTasks: JSON.parse(localStorage.getItem(inProgressTasksKey) || '[]'), 
                 doneTasks: JSON.parse(localStorage.getItem(doneTasksKey) || '[]')
             }
@@ -28,34 +29,61 @@ const setItems = (_tasks, key) => {
     localStorage.setItem(key, JSON.stringify(_tasks));
 }
 
-const showCards = () =>  {
-    const inProgressList = document.querySelector('.in-progress-tasks-list')
-    const doneList = document.querySelector('.done-tasks-list')
-    let { inProgressTasks, doneTasks } = getItems()
-
-    inProgressList.innerHTML = ''
-    doneList.innerHTML = ''
+const generateCards = (taskList, documentList) => {
     const day = new Date();
 
-    inProgressTasks.forEach(task => {
-        inProgressList.innerHTML += `<li style="background: var(--bg-body); word-wrap: wrap;">
-                                    <div class="task-header" style="display:flex; justify-content: space-between;">
-                                        <span style="color:gray; font-size:0.8rem;">${day.toLocaleDateString('pt-PT')}</span>
-                                        <span style="font-weight: 700;font-size: 0.8rem;border-radius:1rem; background-color:${colorsBackground[task.priority]}; padding: 0.2rem 1rem; color:${colors[task.priority]}">${task.priority} Level</span>
-                                    </div>
-                                    
-                                    <div style="margin-top: 1.2rem; position: relative;">
-                                        <p>${task.task_name}</p>
-                                        <button style="background:transparent; outline:none; border: 0; cursor: pointer; position: absolute; right: 0;"><img style="width: 1rem; height: 1rem;" src="./assets/img/angle-right.png"></button>
-                                    <div>
-                                </li>`
-    }); 
+    taskList.forEach(task => {
+        documentList.innerHTML += `<li style="background: var(--bg-body); word-wrap: wrap;">
+            <div class="task-header" style="display:flex; justify-content: space-between;">
+                <span style="color:gray; font-size:0.8rem;">${day.toLocaleDateString('pt-PT')}</span>
+                <span style="font-weight: 700;font-size: 0.8rem;border-radius:1rem; background-color:${colorsBackground[task.priority]}; padding: 0.2rem 1rem; color:${colors[task.priority]}">${task.priority} Level</span>
+            </div>
+            
+            <div style="margin-top: 1.2rem; position: relative;">
+                <p>${task.task_name}</p>
+                <button 
+                onclick="moveCard('${task.task_name}','${task.status}')"
+                style="background:transparent; outline:none; border: 0; cursor: pointer; position: absolute; right: 0;"><img style="width: 1rem; height: 1rem;" src="./assets/img/angle-right.png"></button>
+                <button onclick="removeCard('${task.task_name}','${task.status}')"
+                style="padding-bottom: 0.3rem; background:transparent; outline:none; border: 0; cursor: pointer; position: absolute; right: 0; margin-right: 1.5rem;">
+                <img style="width: 1rem; height: 1rem;" src="./assets/img/delete.png"/></button>
+            <div>
+        </li>`
+    })
+    
+}
+
+const showCards = () =>  {
+    const toDoList = document.querySelector('.to-do-tasks-list')
+    const inProgressList = document.querySelector('.in-progress-tasks-list')
+    const doneList = document.querySelector('.done-tasks-list')
+    let { toDoTasks, inProgressTasks, doneTasks } = getItems()
+
+    toDoList.innerHTML = ''
+    inProgressList.innerHTML = ''
+    doneList.innerHTML = ''
+    
+    const day = new Date();
+    generateCards(toDoTasks, toDoList)
+    generateCards(inProgressTasks, inProgressList)
 
     doneTasks.forEach(task => {
-        doneList.innerHTML += `<li style="background:${colors[task.priority]}; word-wrap: wrap;">
-                                    ${task.task_name}
-                                    <p>${day.toLocaleDateString('pt-PT')}</p>
-                                </li>`
+        doneList.innerHTML += `<li style="background: var(--bg-body); word-wrap: wrap;">
+        <div class="task-header" style="display:flex; justify-content: space-between;">
+            <span style="color:gray; font-size:0.8rem;">${day.toLocaleDateString('pt-PT')}</span>
+            <span style="font-weight: 700;font-size: 0.8rem;border-radius:1rem; background-color:${colorsBackground[task.priority]}; padding: 0.2rem 1rem; color:${colors[task.priority]}">${task.priority} Level</span>
+        </div>
+        
+        <div style="margin-top: 1.2rem; position: relative;">
+            <p>${task.task_name}</p>
+            <button 
+            onclick="moveCard('${task.task_name}','${task.status}')"
+            style="background:transparent; outline:none; border: 0; cursor: pointer; position: absolute; right: 0;"><img style="width: 1rem; height: 1rem;" src="./assets/img/angle-right.png"></button>
+            <button onclick="removeCard('${task.task_name}','${task.status}')"
+            style="padding-bottom: 0.3rem; background:transparent; outline:none; border: 0; cursor: pointer; position: absolute; right: 0; margin-right: 1.5rem;">
+            <img style="width: 1rem; height: 1rem;" src="./assets/img/delete.png"/></button>
+        <div>
+    </li>`
     })
 
 }
@@ -67,12 +95,14 @@ addButton.addEventListener('click', () => {
     if (!task.value) {
         alert('Please, write some task')
     } else {
-        let { inProgressTasks, doneTasks } = getItems()
-        inProgressTasks.push({
+        let { toDoTasks, inProgressTasks, doneTasks } = getItems()
+        
+        toDoTasks.push({
             task_name: task.value,
-            priority: priority.value
+            priority: priority.value,
+            status: 'to-do'
         })
-        setItems(inProgressTasks, inProgressTasksKey)
+        setItems(toDoTasks, toDoTasksKey)
         showCards()
     }
     task.value = ''
@@ -83,29 +113,54 @@ const getCard = (tasks, titleCard) => {
     return index
 }
 
-const removeCard = (_name, _tasks) => {
-    let { inProgressTasks, doneTasks } = getItems()
-    let index = getCard(inProgressTasks, _name)
-    console.log(index)
-    inProgressTasks.splice(index, 1);
-    setItems(inProgressTasks, inProgressTasksKey)
+const  removeCard = (_name, status) => {
+    let { toDoTasks, inProgressTasks, doneTasks } = getItems()
+    let index
+    if (status === 'to-do') {
+        index = getCard(toDoTasks, _name);
+        toDoTasks.splice(index, 1)
+        setItems(toDoTasks, toDoTasksKey)
+    } else if  (status === 'in-progress') {
+        index = getCard(inProgressTasks, _name);
+        inProgressTasks.splice(index, 1)
+        setItems(inProgressTasks, inProgressTasksKey)
+    } else {
+        index = getCard(toDoTasks, _name);
+        doneTasks.splice(index, 1);
+        setItems(doneTasks, doneTasksKey)
+    }
     showCards()
 }
 
-const doneCard = (_name) => {
-    let {inProgressTasks, doneTasks} = getItems()
-    let index = inProgressTasks.findIndex(task => task.task_name == _name)
-    let titleCard = inProgressTasks[index].task_name
-    let priority = inProgressTasks[index].priority
+const moveCard = (_name, _status) => {
+    let { toDoTasks, inProgressTasks, doneTasks} = getItems()
+   
+    
 
-    doneTasks.push({
-        task_name: titleCard,
-        priority: priority
-    })
-    removeCard(titleCard, inProgressTasks)
-    setItems(doneTasks, doneTasksKey);
+    if (_status === 'to-do') {
+        let index = getCard(toDoTasks, _name)
+        let titleCard = toDoTasks[index].task_name
+        let priority = toDoTasks[index].priority
+
+        inProgressTasks.push({
+            task_name: titleCard,
+            priority: priority,
+            status: 'in-progress'
+        })
+        setItems(inProgressTasks, inProgressTasksKey);
+    } else if (_status === 'in-progress') {
+        let index = getCard(inProgressTasks, _name)
+        let titleCard = inProgressTasks[index].task_name
+        let priority = inProgressTasks[index].priority
+        doneTasks.push({
+            task_name: titleCard,
+            priority: priority,
+            status: 'done'
+        })
+        setItems(doneTasks, doneTasksKey);
+    }
+    removeCard(_name, _status)
     showCards()
 }
-
 
 showCards()
